3d599191f22de09213ace5ffc253ed4e501c0f95
Prasanth Diviti <121868816+Prasanth7184@users.noreply.github.com>
2023-06-30 14:19:15 +0530

    Add files via upload

--- pattern2.java
+++ pattern2.java
@@ -21,6 +21,96 @@ public class pattern2 {
             }
             System.out.println("");
         }
-
+        System.out.println("");
+        int b=5;
+        for(int i=1;i<=b;i++){
+            for(int j=1;j<=b-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=5;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        System.out.println("");
+         for(int i=1;i<=a;i++){
+             for(int j=1;j<=i;j++){
+                 System.out.print("*");
+             }
+             for(int j=1;j<=2*(a-i);j++){
+                 System.out.print(" ");
+             }
+             for(int j=1;j<=i;j++){
+                 System.out.print("*");
+             }
+             System.out.println("");
+         }
+         for(int i=a;i>=1;i--){
+             for(int j=1;j<=i;j++){
+                 System.out.print("*");
+             }
+             for(int j=1;j<=2*(a-i);j++){
+                 System.out.print(" ");
+             }
+             for(int j=1;j<=i;j++){
+                 System.out.print("*");
+             }
+             System.out.println("");
+        }
+         System.out.println("");
+         for(int i=1;i<=a;i++){
+             for(int j=1;j<=a-i;j++){
+                 System.out.print(" ");
+             }
+             for(int j=1;j<=i;j++){
+                 System.out.print(i+" ");
+             }
+             System.out.println("");
+         }
+         System.out.println("");
+         for(int i=1;i<=a;i++){
+             for(int j=1;j<=a-i;j++){
+                 System.out.print(" ");
+             }
+             for(int j=i;j>=1;j--){
+                 System.out.print(j);
+             }
+             if(i>=2){
+                 for(int j=2;j<=i;j++){
+                     System.out.print(j);
+                 }
+             }
+             System.out.println("");
+         }
+         System.out.println("");
+         for(int i=1;i<=a;i++){
+             for(int j=1;j<=a-i;j++){
+                 System.out.print(" ");
+             }
+             for(int j=1;j<=i;j++){
+                 System.out.print("*");
+             }
+             if(i>=2){
+                 for(int j=2;j<=i;j++){
+                     System.out.print("*");
+                 }
+             }
+             System.out.println("");
+         }
+         for(int i=a;i>=1;i--){
+             for(int j=1;j<=a-i;j++){
+                 System.out.print(" ");
+             }
+             for(int j=1;j<=i;j++){
+                 System.out.print("*");
+             }
+             if(i>=2){
+                 for(int j=2;j<=i;j++){
+                     System.out.print("*");
+                 }
+             }
+             System.out.println("");
+         }
     }
 }
+ c
